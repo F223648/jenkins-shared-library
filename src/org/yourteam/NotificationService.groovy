fad56b5d6eaf6caaa9ef6268eb767659339b1cab
Ibraheem <155681864+F223648@users.noreply.github.com>
2026-04-26 06:25:05 -0700

    Fix Slack webhook URL usage in NotificationService

--- src/org/yourteam/NotificationService.groovy
+++ src/org/yourteam/NotificationService.groovy
@@ -10,9 +10,9 @@ class NotificationService implements Serializable {
     void sendSlack(String message, String color = 'good') {
         script.echo "Sending Slack Notification [${color}]: ${message}"
         
-        // Use curl to send the message to the webhook URL
+        // Use single quotes for the JSON payload to prevent shell expansion of brackets []
         def payload = "{\"attachments\": [{\"text\": \"${message}\", \"color\": \"${color}\"}]}"
-        script.sh "curl -X POST -H 'Content-type: application/json' --data '${payload}' ${script.env.SLACK_WEBHOOK}"
+        script.sh "curl -X POST -H 'Content-type: application/json' --data '${payload}' \"\$SLACK_WEBHOOK\""
     }
 
     void sendEmail(String to, String subject, String body) {
